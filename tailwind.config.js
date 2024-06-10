f7780cede2481bc22c9f2902e41125791c8a3898
datamonsterr <phamthanhdat17092004@gmail.com>
2024-06-10 20:22:44 +0700

    add my themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
-/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
 module.exports = {
-  content: ["./view/**/*.templ"],
+  content: ["./view/**/*.{html,templ}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#cccccc",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-300": "#ffffff",
+          "info": "#2094f3",
+          "success": "#009485",
+          "warning": "#ff9900",
+          "error": "#ff5724",
+        },
+      },
+    ],
+  },
 };
